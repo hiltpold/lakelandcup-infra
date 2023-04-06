faae6e4c398346e8ec581017607414aea0e89d6e
hiltpold <m.hiltpold@protonmail.ch>
2023-04-06 10:07:25 +0000

    working docker-compose

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -7,8 +7,8 @@ fi
 
 domains=(lakelandcup.ch www.lakelandcup.ch)
 rsa_key_size=4096
-data_path=""
-email="" # Adding a valid address is strongly recommended
+data_path="/root/lakelandcup/lakelandcup-ui/docker/certbot"
+email="m.hiltpold@protonmail.ch" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
